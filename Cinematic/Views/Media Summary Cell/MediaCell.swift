3a67855ef0f93eb886c0d9ce99c5071caf43dce6
Mohsin Ali Ayub <meismosin@gmail.com>
2023-05-07 19:26:34 +0200

    improve image loading

--- Cinematic/Views/Media Summary Cell/MediaCell.swift
+++ Cinematic/Views/Media Summary Cell/MediaCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class MediaCell: UICollectionViewCell {
     @IBOutlet private weak var mediaNameLabel: UILabel!
@@ -17,33 +18,13 @@ class MediaCell: UICollectionViewCell {
     }
     
     public var posterImage: UIImage? {
-        get { posterImageView.image }
-        set {
-            guard let image = newValue else { mediaNameLabel.isHidden = false; return }
-            DispatchQueue.main.async {
-                self.posterImageView.image = image
-                self.mediaNameLabel.isHidden = true
-            }
-        }
+        posterImageView.image
     }
     
     private let imageDownloader: ImageDownloader = CinematicImageDownloader()
     
     func display(mediaSummary: MediaSummary) {
         mediaNameLabel.text = mediaSummary.label
-        downloadAndDisplayPoster(from: mediaSummary.posterURL)
-    }
-    
-    private func downloadAndDisplayPoster(from url: URL?) {
-        guard let url = url else { return }
-        
-        Task {
-            do {
-                let posterImage = try await self.imageDownloader.downloadImage(from: url)
-                self.posterImage = posterImage
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
+        posterImageView.sd_setImage(with: mediaSummary.posterURL)
     }
 }
