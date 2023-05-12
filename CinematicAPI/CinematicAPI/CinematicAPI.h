418b4c2880b29bf4433385873abc9c8a8545c707
Mohsin Ali Ayub <meismosin@gmail.com>
2023-05-12 11:33:07 +0200

    add API framework for modular code

---
+++ CinematicAPI/CinematicAPI/CinematicAPI.h
@@ -0,0 +1,18 @@
+//
+//  CinematicAPI.h
+//  CinematicAPI
+//
+//  Created by Mohsin Ali Ayub on 12.05.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CinematicAPI.
+FOUNDATION_EXPORT double CinematicAPIVersionNumber;
+
+//! Project version string for CinematicAPI.
+FOUNDATION_EXPORT const unsigned char CinematicAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CinematicAPI/PublicHeader.h>
+
+
